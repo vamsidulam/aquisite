9c076a56d2293a685676b3a4be203ac4637cf7da
vamsidulam <vamsi.dulam@sasi.ac.in>
2026-07-20 18:37:50 +0530

    made changes

--- utils/axiosConfig.ts
+++ utils/axiosConfig.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://api.acquiescent.in/api',
+    baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
     timeout: 5000,
 });
 
